42e8ef215c2d4ec48ef8f29f541c755a5207aba4
Sam Allen <samallen659@gmail.com>
2024-03-09 17:17:13 +0000

    removes cmd diretory for simplier layout

--- main.go
+++ main.go
@@ -7,8 +7,13 @@ import (
 
 func main() {
 	var byteFlag bool
+	var lineFlag bool
+	var fileName string
+	flag.BoolVar(&lineFlag, "l", false, "Show line count of input")
 	flag.BoolVar(&byteFlag, "c", false, "Show byte count of input")
 	flag.Parse()
+	fmt.Println(fileName)
+	fmt.Println(flag.Args())
 
 	if byteFlag {
 		fmt.Println("true")
